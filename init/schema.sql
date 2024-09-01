07cc9042c063ffe54c96c90c7bc325c08fee6a56
Andres Kouvach <akouvach@yahoo.com>
2024-09-01 09:51:19 -0300

    restart

---
+++ init/schema.sql
@@ -0,0 +1,11 @@
+﻿DROP DATABASE IF EXISTS northwind;
+CREATE DATABASE IF NOT EXISTS northwind;
+USE northwind;
+CREATE TABLE `Categories` (
+    `CategoryID` INTEGER NOT NULL AUTO_INCREMENT,
+    `CategoryName` VARCHAR(15) NOT NULL,
+    `Description` MEDIUMTEXT,
+    `Picture` LONGBLOB,
+    CONSTRAINT `PK_Categories` PRIMARY KEY (`CategoryID`)
+);
+CREATE INDEX `CategoryName` ON `Categories` (`CategoryName`);
